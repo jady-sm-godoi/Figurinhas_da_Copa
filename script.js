8f4435780119e06665f3c25048d7e5f0f6cd7ea0
jady-sm-godoi <jady.s.m@gmail.com>
2022-11-12 22:04:40 -0300

    construçao formulario - troca dos icones cartoes

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-/**
- * um array com as skills -> para colocar as opções no formulário
- * no formulario o campo só pode aceitar no máximo 5 skills na seleção
- * um array com as skills selecionadas - no máximo 5
- * pegar a row onde os iskills serão inseridos
- * for cada skill in selecaoskills
- * na row insere o elemento com ${skill}
- * transforma isso em uma função
- * 
- */
+
+let isBlue = true
 const skills = ['python', 'django', 'dart', 'github', 'html', 'css', 'js', 'react-js', 'flutter']
-const selecaoSkills = ['github', 'html', 'css', 'js', 'react-js']
+const selecaoSkills = ['github', 'html', 'css', 'js', 'react-js', 'django','flutter']
+
+function changeCard(event){
+    const card = event.currentTarget;
+    const bgColor = isBlue ? 'green' : 'blue'
+    card.style.background = `url(./assets/bg-${bgColor}.svg)`
+
+    isBlue = !isBlue
+    const row = document.querySelector('.skillsRow')
+    row.innerHTML = ''
+    changeSkillsIcons(selecaoSkills)
+
+}
 
 function addSkillsIcons(skillsArray){
     const row = document.querySelector('.skillsRow')
@@ -21,27 +25,27 @@ function addSkillsIcons(skillsArray){
     })
 }
 
-addSkillsIcons(selecaoSkills)
-
-
-
-let isBlue = true
-
-function changeCard(event){
-    const card = event.currentTarget;
-    const bgColor = isBlue ? 'green' : 'blue'
-    card.style.background = `url(./assets/bg-${bgColor}.svg)`
-    
-    const skill0 = isBlue ? 'python' : 'html'
-    const skill1 = isBlue ? 'django' : 'css'
-    const skill2 = isBlue ? 'dart' : 'js'
-    const skill3 = isBlue ? 'github' : 'react-js'
-    
-    const row = document.querySelector('.skillsRow')
-    row.children[0].src = `./assets/${skill0}-icon.png`
-    row.children[1].src = `./assets/${skill1}-icon.png`
-    row.children[2].src = `./assets/${skill2}-icon.png`
-    row.children[3].src = `./assets/${skill3}-icon.png`
-
-    isBlue = !isBlue
+function changeSkillsIcons(skillsArray){
+    if(skillsArray.length <= 5){
+        addSkillsIcons(skillsArray)
+    }else if(skillsArray.length > 5){
+        const skillsArrayBlue = skillsArray.slice(0,4)
+        const skillsArrayGreen = skillsArray.slice(5)
+        if(isBlue){
+            addSkillsIcons(skillsArrayBlue)
+        }
+        if(!isBlue){
+            addSkillsIcons(skillsArrayGreen)
+        }
+    }
 }
+
+changeSkillsIcons(selecaoSkills)
+
+
+/**
+ * funcao: pega os valores dos inputs do formulario e constroi um objeto
+ * colocar essa função no onclick do botão Salvar no formulário
+ * atribuir valores aos campos no card de acordo com os itens do objeto criado
+ * consertar os bugs 
+ */
